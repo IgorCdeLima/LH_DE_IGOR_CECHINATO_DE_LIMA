5951f8b0ebed46c035773345fc947f3dfb97f5a7
IgorCdeLima <igor.c.l.oficial@outlook.com>
2025-09-14 09:12:41 -0300

    del func csv_loader

--- scripts/data_warehousing/csv_loader.py
+++ scripts/data_warehousing/csv_loader.py
@@ -43,44 +43,6 @@ class CsvLoader:
         
         print("Table public.transacoes was created and configured")
         
-    def csv_loader(self):
-        try:
-            with open(self.address_csv, "r", encoding="utf-8", newline='') as transacoes:
-                with self.conn.cursor() as cur:   
-                    for i, line in enumerate(transacoes):
-                        line = line.split(",")
-                        if i > 0:
-                            data = {
-                                'cod_transacao' : int(line[0]),
-                                'num_conta' : int(line[1]),
-                                'data_transacao' : str(line[2]),
-                                'nome_transacao' : str(line[3]),
-                                'valor_transacao' : float(line[4])
-                            }
-
-                            cur.execute(
-                                """INSERT INTO public.transacoes(cod_transacao,num_conta,data_transacao,nome_transacao,valor_transacao) 
-                                VALUES (%(cod_transacao)s, %(num_conta)s, %(data_transacao)s, %(nome_transacao)s, %(valor_transacao)s) 
-                                ON CONFLICT (cod_transacao) DO NOTHING
-                                RETURNING cod_transacao;
-                                """, data)
-                            
-                            result = cur.fetchone()
-                            if result:
-                                print(f"Inserted line: {data['cod_transacao']}")
-                            else:
-                                print(f"line not inserted: {data['cod_transacao']}")
-
-        except errors.UndefinedTable as error:
-            raise errors.UndefinedTable(f"the table public.transacoes does not exist")
-        except ProgrammingError as error:
-            raise ProgrammingError(f"error in sql command: {error}")
-        except FileNotFoundError as error:
-            raise FileNotFoundError(f"error searching the path: {error}")
-        except errors.IntegrityError as error:
-            raise errors.IntegrityError(f"integraty error inserting data: {error}")
-        except Exception as error:
-            raise Exception(f"Unexpected error: {error}")
 
 
     def transacao_table(self):
